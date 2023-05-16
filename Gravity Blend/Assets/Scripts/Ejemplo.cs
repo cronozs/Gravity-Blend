31e3a718bf8006d013718733046a7c4572861ec0
Carlos R <crono_zs@hotmail.com>
2023-05-16 10:12:56 -0600

    agregando la condicional para el cambio de gravedad

--- Gravity Blend/Assets/Scripts/Ejemplo.cs
+++ Gravity Blend/Assets/Scripts/Ejemplo.cs
@@ -7,12 +7,15 @@ public class Ejemplo : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private float speed;
     private Rigidbody2D _rb;
+    private SpriteRenderer _sp;
+    private bool canChangeGravity = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,9 +26,11 @@ public class Ejemplo : MonoBehaviour
             gameObject.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && canChangeGravity)
         {
+            _sp.flipY = !_sp.flipY;
             _rb.gravityScale = _rb.gravityScale *-1;
+            canChangeGravity = false;
         }
 
         if (Input.GetButtonDown("Fire1"))
@@ -34,5 +39,11 @@ public class Ejemplo : MonoBehaviour
         }
     }
 
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Floor")
+        {
+            canChangeGravity = true;
+        }
+    }
 }
